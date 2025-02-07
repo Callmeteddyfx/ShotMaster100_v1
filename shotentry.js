7fd54a424113088c8737382f95bf4c9c86b0b22b
Callmeteddyfx <136664394+Callmeteddyfx@users.noreply.github.com>
2025-02-07 02:38:27 -0800

    basic shotentry js done

--- shotentry.js
+++ shotentry.js
@@ -1,3 +1,5 @@
+
+/* Create an FG% progressbar */
 var fgpercent = document.getElementById("livefgpercent");
 
 
@@ -10,24 +12,89 @@ var bar1 = new ProgressBar.Circle(fgpercent, {
 });
 
 function updatefgpercent(bar,value) {
-    bar.animate(value);
-    bar.setText((value*100) + '%');
+    setTimeout(() => {
+        value = parseFloat(value); 
+        value = Number((value).toFixed(2));
+        bar.setText(Math.round(value * 100) + '%');    
+        bar.animate(value);
+    }, 100);
 }
 
 updatefgpercent(bar1, 0);
 
+/* Get today's birthday */
 function todaysDate(){
     const date = new Date().toLocaleDateString();
     const text = document.getElementById('todaysdate');
     text.textContent = date;
 }
 
-
+/*Load today's birthday */
 window.onload = todaysDate;
 
-const layupsvalue = null;
-const midrangevalue = null;
-const threepointervalue = null;
-const cornerthreevalue = null;
-const freethrowvalue = null;
-const freestylevalue = null;
+let points = 0; /* This value stores your points temporarily to calculate your FG% */
+const layupsvalue = document.getElementById('layupsvalue');
+const midrangevalue = document.getElementById('midrangevalue');
+const threepointervalue = document.getElementById('threepointervalue');
+const cornerthreevalue = document.getElementById('cornerthreevalue');
+const freethrowvalue = document.getElementById('freethrowvalue');
+const freestylevalue = document.getElementById('freestylevalue');
+
+/* Function to add and subtract values to stats */
+function addandsubtractnumbers (Id,addbtnId,minbtnId,max){
+    minbtnId.addEventListener( 'click', function(){
+        if(Id.textContent == 0){
+
+        } else {
+            Id.textContent--;
+            points--;
+            updatefgpercent(bar1,(points/100));
+        }
+        }
+    )
+    addbtnId.addEventListener( 'click', function(){
+        if(Id.textContent == max){
+
+        } else {
+            Id.textContent++;
+            points++;
+            updatefgpercent(bar1,(points/100));
+        }
+        }
+    )    
+}
+
+addandsubtractnumbers(layupsvalue,addlayups,minuslayups,15);
+addandsubtractnumbers(midrangevalue,addmidrange,minusmidrange,15);
+addandsubtractnumbers(threepointervalue,addthreepointer,minusthreepointer,20);
+addandsubtractnumbers(cornerthreevalue,addcornerthree,minuscornerthree,15);
+addandsubtractnumbers(freethrowvalue,addfreethrows,minusfreethrows,15);
+addandsubtractnumbers(freestylevalue,addfreestylevalue,minusfreestyle,20);
+
+
+
+/* Reset Session */
+document.getElementById('resetbtn').addEventListener( 'click', function(){
+    let resetdiv = document.getElementById('resetwrapper');
+    resetdiv.style.display = 'block';
+    }
+)
+document.getElementById('cancel-reset').addEventListener( 'click', function(){
+    let resetdiv = document.getElementById('resetwrapper');
+    resetdiv.style.display = 'none';
+    }
+)
+document.getElementById('proceed-reset').addEventListener( 'click', function(){
+    let resetdiv = document.getElementById('resetwrapper');
+    resetdiv.style.display = 'none';
+
+    points = 0;
+    updatefgpercent(bar1, 0);
+    layupsvalue.textContent = 0;
+    midrangevalue.textContent = 0;
+    threepointervalue.textContent = 0;
+    cornerthreevalue.textContent = 0;
+    freethrowvalue.textContent = 0;
+    freestylevalue.textContent = 0;
+    }
+)
